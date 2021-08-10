47560e9d4443d71e91cfe807d16681a49d1adaae
Samuel Anumudu <samuelmaize55@yahoo.com>
2021-08-10 18:52:44 +0200

    first commit - Setup

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const request = require("request");
+
+// initialize a new express app
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Serving running on port 3000");
+});
